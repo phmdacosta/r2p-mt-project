274d67b7dbab78d8dd6e93ee273730d0e6b19b14
pedroh.mdacosta@gmail.com <pedroh.mdacosta@gmail.com@37f8aa7a-451f-7ca3-09dc-02c55fd3737a>
2014-12-24 03:39:21 +0000

    Pedro Henrique:
    Compartilhando o projeto criado no repositório.

---
+++ MTProject/src/br/com/r2psoft/mtproject/MainActivity.java
@@ -0,0 +1,15 @@
+package br.com.r2psoft.mtproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class MainActivity extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
