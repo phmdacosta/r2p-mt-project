b20c4d17c454112d971bc5f2b66344556f37de74
pedroh.mdacosta@gmail.com <pedroh.mdacosta@gmail.com@37f8aa7a-451f-7ca3-09dc-02c55fd3737a>
2014-12-24 03:39:21 +0000

    Pedro Henrique:
    Compartilhando o projeto criado no repositório.

---
+++ trunk/MTProject/src/br/com/r2psoft/mtproject/SearchMapActivity.java
@@ -0,0 +1,13 @@
+package br.com.r2psoft.mtproject;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class SearchMapActivity extends Activity {
+
+	@Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search_map);
+    }
+}
