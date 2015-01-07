4af274187ebc342a7fdbed7db1f568a8a1ddf2fe
pedroh.mdacosta@gmail.com <pedroh.mdacosta@gmail.com@37f8aa7a-451f-7ca3-09dc-02c55fd3737a>
2015-01-07 01:09:19 +0000

    Pedro Henrique:
    Adicionando as classes de entidade e organizando a MainActivity.

---
+++ trunk/MTProject/src/br/com/r2psoft/entities/ImagemBanheiro.java
@@ -0,0 +1,88 @@
+/**
+ * 
+ */
+package br.com.r2psoft.entities;
+
+/**
+ * Classe da entidade Imagem.
+ * @author Pedro Henrique
+ */
+public class ImagemBanheiro {
+
+	private long id;
+	private byte imagem;
+	private boolean flagAtiva;
+	private long tamArqImagem;
+	private String tipoArqImagem;
+	
+	
+	/**
+	 * @return the id
+	 */
+	public long getId() {
+		return id;
+	}
+	
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(long id) {
+		this.id = id;
+	}
+	
+	/**
+	 * @return the imagem
+	 */
+	public byte getImagem() {
+		return imagem;
+	}
+	
+	/**
+	 * @param imagem the imagem to set
+	 */
+	public void setImagem(byte imagem) {
+		this.imagem = imagem;
+	}
+	
+	/**
+	 * @return the flagAtiva
+	 */
+	public boolean isFlagAtiva() {
+		return flagAtiva;
+	}
+	
+	/**
+	 * @param flagAtiva the flagAtiva to set
+	 */
+	public void setFlagAtiva(boolean flagAtiva) {
+		this.flagAtiva = flagAtiva;
+	}
+	
+	/**
+	 * @return the tamArqImagem
+	 */
+	public long getTamArqImagem() {
+		return tamArqImagem;
+	}
+	
+	/**
+	 * @param tamArqImagem the tamArqImagem to set
+	 */
+	public void setTamArqImagem(long tamArqImagem) {
+		this.tamArqImagem = tamArqImagem;
+	}
+	
+	/**
+	 * @return the tipoArqImagem
+	 */
+	public String getTipoArqImagem() {
+		return tipoArqImagem;
+	}
+	
+	/**
+	 * @param tipoArqImagem the tipoArqImagem to set
+	 */
+	public void setTipoArqImagem(String tipoArqImagem) {
+		this.tipoArqImagem = tipoArqImagem;
+	}
+}
--- trunk/MTProject/src/br/com/r2psoft/mtproject/MainActivity.java
+++ trunk/MTProject/src/br/com/r2psoft/mtproject/MainActivity.java
@@ -2,8 +2,6 @@ package br.com.r2psoft.mtproject;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 public class MainActivity extends Activity {
 
@@ -12,4 +10,5 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+    
 }
