d0ca67add64e0587965af95b54e0562cd8dc40ee
portela.vp@gmail.com <portela.vp@gmail.com@37f8aa7a-451f-7ca3-09dc-02c55fd3737a>
2015-01-15 23:51:45 +0000

    segue projeto atualizado com o MAPA. coloquei 2 txt em anexo com a chave que precisamos pra acessar o mapa no google e outro com a info de latitude e longitude do meu bairro.

--- trunk/MTProject/src/br/com/r2psoft/mtproject/MainActivity.java
+++ trunk/MTProject/src/br/com/r2psoft/mtproject/MainActivity.java
@@ -1,14 +1,29 @@
 package br.com.r2psoft.mtproject;
 
-import android.app.Activity;
-import android.os.Bundle;
 
-public class MainActivity extends Activity {
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        
+        Button Procurar = (Button) findViewById(R.id.btnProcurar);
+        Procurar.setOnClickListener(this);
     }
+
+	@Override
+	public void onClick(View v) {
+		// TODO Auto-generated method stub
+		Intent it = new Intent(this, SearchMapActivity.class);
+		startActivity(it);
+	}     
     
 }
