59338025778d3c69ec20ce03786cda928f2f96a3
portela.vp@gmail.com <portela.vp@gmail.com@37f8aa7a-451f-7ca3-09dc-02c55fd3737a>
2015-01-15 23:51:45 +0000

    segue projeto atualizado com o MAPA. coloquei 2 txt em anexo com a chave que precisamos pra acessar o mapa no google e outro com a info de latitude e longitude do meu bairro.

--- MTProject/src/br/com/r2psoft/mtproject/SearchMapActivity.java
+++ MTProject/src/br/com/r2psoft/mtproject/SearchMapActivity.java
@@ -1,13 +1,26 @@
 package br.com.r2psoft.mtproject;
 
-import android.app.Activity;
 import android.os.Bundle;
 
-public class SearchMapActivity extends Activity {
+import com.google.android.gms.maps.CameraUpdate;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
+public class SearchMapActivity extends android.support.v4.app.FragmentActivity {
 
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+    protected void onCreate(Bundle icicle) {
+        super.onCreate(icicle);
         setContentView(R.layout.activity_search_map);
+        SupportMapFragment mapFragment = (SupportMapFragment)
+        		getSupportFragmentManager().findFragmentById(R.id.map);
+        GoogleMap map = mapFragment.getMap();
+        
+        map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        LatLng latlng = new LatLng(-22.8440802,-43.3094623);
+        CameraUpdate update = CameraUpdateFactory.newLatLngZoom(latlng, 15);
+        map.moveCamera(update);
     }
 }
